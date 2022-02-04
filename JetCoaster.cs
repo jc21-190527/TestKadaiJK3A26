0f5f915f89a1c1d13febc01d3484a8d84ba569ec
大樹 髙橋 <190527@jc-21.jp>
2022-02-04 14:50:29 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testkadai
+{
+   public class JetCoaster
+    {
+        public static int ride(int k, int[] g, int r)
+        {
+            int index = 0;
+            int totalcount = 0;
+            for (int i = 0; i < r; i++)
+            {
+                int ridecount = k;
+                for (int j = 0; j < g.Length; j++)
+                {
+                    if (ridecount >= g[index])
+                    {
+                        ridecount = ridecount - g[index];
+                        index++;
+                        index = index % g.Length;
+                    }
+                    else
+                    {
+
+
+
+                        break;
+                    }
+
+
+
+                }
+                totalcount = totalcount + (k - ridecount);
+            }
+            return totalcount;
+        }
+    }
+}
---
+++ kuriage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testkadai
+{
+    public class kuriage
+    {
+        public static int kuriage10(int i)
+        {
+            int result = i;
+            bool flg = false;
+            if (i < 0)
+            {
+                result = Math.Abs(i);
+                flg = true;
+            }
+            result = (int)(Math.Ceiling(result * 0.1) * 10);
+
+            if (flg)
+            {
+                result *= -1;
+            }
+            return result;
+        }
+    }
+
+}
